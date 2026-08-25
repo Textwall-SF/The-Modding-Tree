79fec348623b0af3e7cd3d8124f3e24f677b2dd1
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-25 18:18:07 +0800

    #0.011

--- js/layers/2.js
+++ js/layers/2.js
@@ -10,7 +10,7 @@ addLayer("w", {
     requires: new Decimal(5000), // Can be a function that takes requirement increases into account
     resource: "walls", // Name of prestige currency
     baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.p.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.3, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -25,5 +25,62 @@ addLayer("w", {
     hotkeys: [
         {key: "w", description: "W: walls", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return hasUpgrade("p",34)}
+    layerShown(){return hasUpgrade("p",34)},
+	upgrades: {
+		11: {
+			title: "TextWall, Start!",
+			description: "+100 point base".
+			cost: new Decimal(1)
+		},
+		12: {
+			title: "Login / Register",
+			description: "Walls boost points.".
+			cost: new Decimal(2),
+			effect() {
+            return player.w.points.add(1).pow(0.4)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+			unlocked() {return hasUpgrade("w",11)}
+		},
+		13: {
+			title: "How to use...",
+			description: "*4 point gain",
+		    tooltip: "tw.2s4.me?",
+			cost: new Decimal(2),
+			unlocked() {return hasUpgrade("w",12)}
+		},
+		14: {
+			title: "Use basic chars",
+			description: "*2.5 point gain",
+		    tooltip: `Use ., -, /, ', + for basics in tw.2s4.me`,
+			cost: new Decimal(4),
+			unlocked() {return hasUpgrade("w",13)}
+		},
+		21: {
+			title: "2020 Textwall",
+			description: "+2020 point gain base",
+		    tooltip: "Textwall was created in 2020"",
+			cost: new Decimal(7),
+			unlocked() {return hasUpgrade("w",14)}
+		},
+		22: {
+			title: "Enter 2021!",
+			description: "*2.021 point gain",
+		    tooltip: "the historic era of textwall, 2021. there is click yes",
+			cost: new Decimal(12),
+			unlocked() {return hasUpgrade("w",21)}
+		},
+		23: {
+			title: "Create subwalls",
+			description: "*5 PP gain and *3 Walls gain",
+			cost: new Decimal(25),
+			unlocked() {return hasUpgrade("w",22)}
+		},
+		24: {
+			title: "Type 50,000 chars of text",
+			description: "*100 point gain",
+			cost: new Decimal(80),
+			unlocked() {return hasUpgrade("w",23)}
+		},
+	}
 })
