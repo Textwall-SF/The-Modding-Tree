997b7795aa890717ffba90a89cc26db79d30f197
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-25 19:07:37 +0800

    #0.011

--- js/layers/2.js
+++ js/layers/2.js
@@ -15,6 +15,7 @@ addLayer("w", {
     exponent: 0.3, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+		if (hasUpgrade(this.layer, 23)) mult = mult.times(3)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
