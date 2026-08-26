f9d56ac32a205f2f4a99e9b97bf440f46eb0b025
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-26 22:19:05 +0800

    #0.024

--- js/layers/1.js
+++ js/layers/1.js
@@ -24,7 +24,8 @@ addLayer("p", {
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
 		exp = new Decimal(1)
-		if(inChallenge("t",11)) exp = exp.pow(0.25)
+		if (inChallenge("t", 11)) exp = exp.pow(0.25)
+		if (hasUpgrade("t", 32)) exp = exp.pow(1.01)
         return exp
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
