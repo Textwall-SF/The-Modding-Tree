9d1f583fb2fff9d6340ec0b8506b54f5a43eea74
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-26 13:52:37 +0800

    #0.021

--- js/layers/2.js
+++ js/layers/2.js
@@ -114,3 +114,32 @@ addLayer("w", {
 		},
 	}
 })
+addLayer("t", {
+    name: "Textcoin", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FFBB00",
+    requires: new Decimal(1e16), // Can be a function that takes requirement increases into account
+    resource: "textcoins", // Name of prestige currency
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.p.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.01, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    branches: ['p'],
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "T: textcoins", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade("w",34) || player.t.unlocked},
+})
