c56d5c1099f7f383027cf56a6918cb3eb796e3cb
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-27 13:11:54 +0800

    #0.101

---
+++ js/layers/3.js
@@ -0,0 +1,57 @@
+addLayer("tw", {
+    name: "textwallers", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "TW", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		    points: new Decimal(0),
+    }},
+    color: "#00FF00",
+    requires: new Decimal(6000), // Can be a function that takes requirement increases into account
+    resource: "textwallers", // Name of prestige currency
+    baseResource: "textcoins", // Name of resource prestige is based on
+    baseAmount() {return player.t.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    base: 30000,
+    exponent: 1.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    branches: ['w', 't'],
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "T", description: "Shift + T: textwallers", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade("t",41) || player.tw.unlocked},
+    milestones: {
+      1: {
+      requirementDescription: "1 textwallers",
+      done() { return player.tw.points.gte(1);}, // Used to determine when to give the milestone
+      effectDescription: "x30 points.",
+      },
+      2: {
+      requirementDescription: "2 textwallers",
+      done() { return player.tw.points.gte(2);}, // Used to determine when to give the milestone
+      effectDescription: "x10 prestige points and walls.",
+      },
+      3: {
+      requirementDescription: "3 textwallers",
+      done() { return player.tw.points.gte(3);}, // Used to determine when to give the milestone
+      effectDescription: "x3 textcoins, unlock new textcoin challenge.",
+      },
+      4: {
+      requirementDescription: "5 textwallers",
+      done() { return player.tw.points.gte(5);}, // Used to determine when to give the milestone
+      effectDescription: "Passively gain 1% of walls per second.",
+      },
+      5: {
+      requirementDescription: "7 textwallers",
+      done() { return player.tw.points.gte(7);}, // Used to determine when to give the milestone
+      effectDescription: "x10,000 points and ^1.01 points.",
+      },
+    }
+})
