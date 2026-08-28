e11ec007e086d543d341c1c95ef3069d827cd2aa
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-28 14:13:33 +0800

    #0.111

--- js/layers/3.js
+++ js/layers/3.js
@@ -12,8 +12,8 @@ addLayer("tw", {
     baseResource: "textcoins", // Name of resource prestige is based on
     baseAmount() {return player.t.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    base: 2.5,
-    exponent: 1.5, // Prestige currency exponent
+    base: 2.2,
+    exponent: 1.3, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -53,5 +53,100 @@ addLayer("tw", {
       done() { return player.tw.points.gte(7);}, // Used to determine when to give the milestone
       effectDescription: "x10,000 points and ^1.01 points.",
       },
+	  6: {
+      requirementDescription: "8 textwallers",
+      done() { return player.tw.points.gte(8);}, // Used to determine when to give the milestone
+      effectDescription: "Passively gain 1% of textcoins per second.",
+      },
+      5: {
+      requirementDescription: "10 textwallers",
+      done() { return player.tw.points.gte(10);}, // Used to determine when to give the milestone
+      effectDescription: "Unlock multiplier.",
+      },
     }
+})
+addLayer("x", {
+    name: "multiplier", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "×", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    branches: ["tw"],
+    color: "#F00",
+    requires: new Decimal(50), // Can be a function that takes requirement increases into account
+    resource: "multiplier", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.1, // Prestige currency exponent
+	infoboxes:{
+            lore: {
+                title: "Credit",
+                body: "Credit to @unicodes (galaxy) for MetaTree, adding Multiplier",
+            }
+        },
+    tabFormat: {
+        "Multiplier": {
+            content: [
+                "main-display",
+                ["display-text", function() {return "You are gaining " + format(mg) + " multiplier per second"}],
+                "blank",
+                "upgrades"
+            ],
+		"Credit": {
+			content: [
+				"infoboxes"
+			],
+		    },
+        },
+    },
+    upgrades: {
+        11: {
+            title: "It's back",
+            description: "x5 point and multiplier gain",
+            cost: new Decimal(20),
+        },
+        12: {
+            title: "MetaTree is Back",
+            description: "x10 point and multiplier gain, also x56.14 textcoins",
+            cost: new Decimal(100),
+            unlocked() {return hasUpgrade('x',11)}
+        },
+        13: {
+            title: `Multiplier<sup>Multiplier</sup>`,
+            description: "Multiply multiplier gain by multiplier",
+            effect() {
+                return player.x.points.add(1).pow(0.05)
+            },
+            effectDisplay() {return 'x' + format(upgradeEffect(this.layer, this.id))},
+            cost: new Decimal(500),
+            unlocked() {return hasUpgrade('x',12)}
+        },
+		14: {
+            title: "What?",
+            description: "*61,729 walls",
+            cost: new Decimal(5e3),
+            unlocked() {return hasUpgrade('x',13)}
+        },
+    },
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    update(diff){
+        let gain = new Decimal(0)
+        if (player.tw.points.gte(10)) gain = gain.add(1)
+        if (hasUpgrade('x', 11)) gain = gain.times(5)
+        if (hasUpgrade('x', 12)) gain = gain.times(10)
+        if (hasUpgrade('x', 13)) gain = gain.times(upgradeEffect('x', 13))
+        mg = gain
+        player.x.points = player.x.points.add(gain.times(diff))
+    },
+    row: 2,
+	layerShown(){return player.tw.points.gte(10) || player.x.unlocked}
 })
