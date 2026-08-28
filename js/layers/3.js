d70cfba4764e6c7714acaadba76391c99545c77b
RenaultFan auth <jeramiemreble12@gmail.com>
2026-08-28 17:20:26 +0800

    #0.111

--- js/layers/3.js
+++ js/layers/3.js
@@ -12,8 +12,8 @@ addLayer("tw", {
     baseResource: "textcoins", // Name of resource prestige is based on
     baseAmount() {return player.t.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    base: 2.2,
-    exponent: 1.3, // Prestige currency exponent
+    base: 2,
+    exponent: 1.25, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -64,4 +64,30 @@ addLayer("tw", {
       effectDescription: "Unlock multiplier.",
       },
     }
+})
+addLayer("x", {
+    name: "multiplier", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "×", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		    points: new Decimal(0),
+    }},
+    color: "#FF0000",
+    requires: new Decimal(0), // Can be a function that takes requirement increases into account
+    resource: "multiplier", // Name of prestige currency
+    baseResource: "textwallers", // Name of resource prestige is based on
+    baseAmount() {return player.tw.points}, // Get the current amount of baseResource
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    branches: ['tw'],
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true}
 })
